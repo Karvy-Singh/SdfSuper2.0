56d144c396b3d88376ede52f5ca30b7dccecff3f
Harsh Sharma <harsh@codelif.in>
2025-03-12 18:56:14 +0530

    fix: from_string has been dropped in boost 1.87.0
    it had been deprecated in previous versions already

--- client.cpp
+++ client.cpp
@@ -37,7 +37,7 @@ int main()
         tcp::socket socket(io);
 
         // 2) Connect to the server
-        socket.connect(tcp::endpoint(boost::asio::ip::address::from_string("127.0.0.1"), 8888));
+        socket.connect(tcp::endpoint(boost::asio::ip::make_address("127.0.0.1"), 8888));
         std::cout << "Connected to server.\n";
 
         // 3) Ask for a username
